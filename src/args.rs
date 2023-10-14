759b23eb6689c78bce8b85b547be7cc2f954aba2
ttyS3 <ttys3.rust@gmail.com>
2023-10-14 12:52:50 +0800

    refactor: refine cli flag comments

--- src/args.rs
+++ src/args.rs
@@ -18,7 +18,7 @@ pub struct Arguments {
     #[clap(long, short, value_enum, default_value_t = Modes::Fill)]
     pub mode: Modes,
 
-    /// Call custom wallpaper command
+    /// Call custom wallpaper command, this flag can repeat multiple times
     #[clap(long, short)]
     pub custom_command: Option<Vec<String>>,
 
@@ -26,12 +26,27 @@ pub struct Arguments {
     #[clap(long, short)]
     pub backup_dir: Option<String>,
 
-    /// Specify the market to use for the wallpaper
-    /// see https://github.com/neffo/bing-wallpaper-gnome-extension/blob/64d516aaf17fda563e4dd2f856e6fa6fa5edc176/extension.js#L628
+    /// Specify the market to use for the wallpaper.
+    /// Available mkt:
+    ///     auto, ar-XA, da-DK, de-AT, de-CH, de-DE, en-AU, en-CA, en-GB, en-ID,
+    ///     en-IE, en-IN, en-MY, en-NZ, en-PH, en-SG, en-US, en-WW, en-XA, en-ZA,
+    ///     es-AR, es-CL, es-ES, es-MX, es-US, es-XL, et-EE, fi-FI, fr-BE, fr-CA,
+    ///     fr-CH, fr-FR, he-IL, hr-HR, hu-HU, it-IT, ja-JP, ko-KR, lt-LT, lv-LV,
+    ///     nb-NO, nl-BE, nl-NL, pl-PL, pt-BR, pt-PT, ro-RO, ru-RU, sk-SK, sl-SL,
+    ///     sv-SE, th-TH, tr-TR, uk-UA, zh-CN, zh-HK, zh-TW
     #[clap(long = "mkt")]
     pub market: Option<String>,
 
-    /// Specify the resolution to use for the wallpaper
+    /// Specify the resolution to use for the wallpaper.
+    /// Available resolution:
+    ///     auto
+    ///     UHD
+    ///     1920x1200
+    ///     1920x1080
+    ///     1366x768
+    ///     1280x720
+    ///     1024x768
+    ///     800x600
     #[clap(long, short)]
     pub resolution: Option<String>,
 }
